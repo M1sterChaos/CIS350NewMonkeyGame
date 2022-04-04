91dc07d483b80544dc485cc4ec3b16b9beab006a
M1sterChaos <austinbuck70@gmail.com>
2022-04-04 15:41:00 -0500

    Fix the up down movement somehow

--- MonkeyGame/Assets/Scripts/TempPlayerMove.cs
+++ MonkeyGame/Assets/Scripts/TempPlayerMove.cs
@@ -10,16 +10,25 @@ public class TempPlayerMove : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.W))
         {
-            if(this.transform.position.y < 5)
+            if(this.transform.position.y < 4.5f)
             {
                 this.transform.Translate(new Vector3(0, 3f, 0));
+                if (this.transform.position.y >= 4.5f)
+                {
+                    transform.position = new Vector3(transform.position.x, 4.5f, transform.position.z);
+                }
+                
             }
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            if (this.transform.position.y > -5)
+            if (this.transform.position.y > -4.5f)
             {
                 this.transform.Translate(new Vector3(0, -3f, 0));
+                if (this.transform.position.y <= -4.5f)
+                {
+                    transform.position = new Vector3(transform.position.x, -4.5f, transform.position.z);
+                }
             }
         }
     }
